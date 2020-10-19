74766b8cf0c9f10e01e134286d51736bf781cf64
TheyKay <murraykyleb@gmail.com>
2020-10-18 22:32:56 -0600

    connection updated to be flexible based on if running on heroku with JAWS

--- config/connection.js
+++ config/connection.js
@@ -3,13 +3,18 @@ var mysql = require("mysql");
 require("dotenv").config();
 var my = require("./my.js");
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: my.pass.word,
-    database: "burgers_db"
-});
+var connection;
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: my.pass.word,
+        database: "burgers_db"
+    });
+};
 
 connection.connect((err) => {
     if (err) {
