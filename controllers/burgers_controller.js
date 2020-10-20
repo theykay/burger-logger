bf233b63d81a09d17c0d36a4bf3db55db69ddfb4
TheyKay <murraykyleb@gmail.com>
2020-10-19 20:42:28 -0600

    burgers_controller routes created and theoretically completed

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,5 +1,36 @@
+// complete...
 const express = require("express");
-var burgers = require("../models/burger");
+var router = express.router();
 
-//create router
-//module.exports = router;
+var burger = require("../models/burger");
+
+router.get("/", (req, res) => {
+    burger.selectAll((data) => {
+        var hbsObject = {
+            burgers: data
+        };
+        res.render("index", hbsObject);
+    })
+});
+
+router.post("/api/burgers", (req, res) => {
+    burger.insertOne(
+        ["burger_name"],
+        [req.body.burger_name],
+        (result) => {
+            res.json( { id: result.insertID })
+        });
+});
+
+router.put("/api/burgers/:id", (req, res) => {
+    var condition = "id = " + req.params.id;
+    burger.updateOne({ devoured: true }, condition, (result) => {
+        if (result.changedRows == 0) {
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    })
+});
+
+module.exports = router;
