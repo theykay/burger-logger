1e30794b412e133de466675cd0e8055ee5270054
TheyKay <murraykyleb@gmail.com>
2020-10-19 20:16:55 -0600

    burger.js in models completed, extending orm and also exported

--- models/burger.js
+++ models/burger.js
@@ -1,3 +1,22 @@
+// complete...
 var orm = require("../config/orm");
 
-// module.exports =
+var burger = {
+    selectAll: (cb) => {
+        orm.selectAll("burgers", (res) => {
+            cb(res);
+        });
+    },
+    insertOne: (cols, vals, cb) => {
+        orm.insertOne("burgers", cols, vals, (res) => {
+            cb(res);
+        })
+    },
+    updateOne: (objColVals, condition, cb) => {
+        orm.updateOne("burgers", objColVals, condition, (res) => {
+            cb(res);
+        })
+    }
+};
+
+module.exports = burger;
