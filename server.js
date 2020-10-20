efaaf025195268243db563f25d5f132cfbc07733
TheyKay <murraykyleb@gmail.com>
2020-10-19 20:45:23 -0600

    routes required in server.js file, server theoretically completed

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
+// completed...
 const express = require("express");
+var exphbs = require("express-handlebars")
+var routes = require("./controllers/burgers_controller")
 
 var app = express();
 var PORT = process.env.PORT || 8080;
@@ -9,6 +12,7 @@ app.use(express.static("/public"));
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
+app.use(routes);
 
 app.listen(PORT, () => {
     console.log("Server listening on: " + PORT);
