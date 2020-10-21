e05f58469004e3ce805c1da68ad98832f7c1dac1
TheyKay <murraykyleb@gmail.com>
2020-10-21 17:22:46 -0600

    changed connection back to autopopulate for deployed database

--- config/connection.js
+++ config/connection.js
@@ -6,14 +6,14 @@ var my = require("./my.js");
 
 var connection;
 if (process.env.JAWSDB_URL) {
-    // connection = mysql.createConnection(process.env.JAWSDB_URL);
-    connection = mysql.createConnection({
-        host: "vrk7xcrab1wsx4r1.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-        port: 3306,
-        user: whlq420lpl2ezw3d,
-        password: my.pass.jaw,
-        database: j3g9eibktreaf5jk
-    })
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+    // connection = mysql.createConnection({
+    //     host: "vrk7xcrab1wsx4r1.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    //     port: 3306,
+    //     user: whlq420lpl2ezw3d,
+    //     password: my.pass.jaw,
+    //     database: j3g9eibktreaf5jk
+    // })
 } else {
     connection = mysql.createConnection({
         host: "localhost",
